3a7762b4a974063f448a60a0a134147aa82da704
Zachary Astree <zacharyastree@gmail.com>
2021-07-24 15:34:26 -0400

    Small changes

--- DeployDocker/app/Http/Controllers/PostsController.php
+++ DeployDocker/app/Http/Controllers/PostsController.php
@@ -22,9 +22,6 @@ class PostsController extends Controller
 
         $imagePath = request('image')->store('uploads','public');
 
-        //$image = Image::make(public_path("storage/{$imagePath}"))->fit(1200,1200);
-        //$image->save();
-
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
             'title' => $data['title'],
